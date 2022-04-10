c96100cdebeb7f67030d3237fdf73de2a15f6fa6
Kajetan Terlecki <k.terlecki@wp.pl>
2022-04-10 20:42:16 +0200

    Główne menu ready

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@
 
 import "./sass/style.scss";
 import {swiperMain, swiperProducts, swiperYours} from "./js/vendor/swiper";
-console.log('działa');
-
 
 swiperMain;
 swiperProducts;
 swiperYours;
+
+const section = document.querySelector('.section--menu');
+const menuJs = document.querySelectorAll('.menuJs');
+
+menuJs.forEach(item => {
+    item.addEventListener('click', e => {
+        e.preventDefault;
+        section.classList.toggle('active');
+    });
+})
