a43b389be84ca228cc74082fd856ddf74918f08c
Kajetan Terlecki <k.terlecki@wp.pl>
2022-05-04 17:09:54 +0200

    drobne zmiany

--- server_config.js
+++ server_config.js
@@ -1,8 +1,17 @@
 "use strict";
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require("express");
 const path = require("path");
 const port = 3000;
 const app = express();
+const strapiURL = 'http://localhost:1337/api';
+
+
+const getApi = function (route) {
+  const fetch_response = await fetch(strapiURL + route);
+  const json = await fetch_response.json();
+  console.log(res.json(json));
+}
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -13,7 +22,8 @@ app.use((req, res, next) => {
 
 app.use("/public", express.static(path.join(__dirname + "/public"))); //folder z plikami udostępnionymi na zewnątrz
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  getApi('/homepages');
   res.show("/index.html");
 });
 
@@ -28,6 +38,8 @@ app.get("/rowery", (req, res) => {
 app.get("/produkt", (req, res) => {
   res.show("/produkt.html");
 });
+
+
 
 const PORT = process.env.PORT || port;
 app.listen(PORT, () => {
